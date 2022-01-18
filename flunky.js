cc265d3d60c8b48ded92cd01ab0386af959032bc
Chuck <41870678+yourdesk@users.noreply.github.com>
2022-01-18 15:33:14 -0500

    add utility functions to main file

--- flunky.js
+++ flunky.js
@@ -1,3 +1,14 @@
+const randFloat = (min, max) => Math.random() * (max - min) + min;
+const randInt = (min, max) => Math.floor(randFloat(min, max));
+const choice = array => array[randInt(0, array.length)];
+const choices = function (array, amt, joined = false, delim = '') {
+  let temp = [];
+  for (let i = 0; i < amt; i++) {
+    temp.push(choice(array));
+  }
+  return joined ? temp.join(delim) : temp;
+};
+
 const DiceFlags = {
     None: 0,
     TakeHighest: 1,
